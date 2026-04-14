1bd305ad66e194719f83ab165a49d39632604db7
aevaksnes <developing.aevaksnes@gmail.com>
2026-04-14 12:23:03 +0200

    chore: remove node_modules and .env from history

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCquJwgiiMLMw3GZxkQ53Rg_mVd3eevuDQ",
-  authDomain: "recipe-dieting-from-inside-out.firebaseapp.com",
-  projectId: "recipe-dieting-from-inside-out",
-  storageBucket: "recipe-dieting-from-inside-out.firebasestorage.app",
-  messagingSenderId: "322081475047",
-  appId: "1:322081475047:web:125b4d6a061f803443d542"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig)
